a51bc22a06b44e399c037bdf09a31719672bf633
NIcolasBonamy <n.bonamy@live.fr>
2020-12-14 18:01:31 +0100

    modify projects pictures path

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import { Component } from "react";
+import axios from "axios";
+import Project from "./Project";
+import styled from "styled-components";
 
-const projects = [
-    {
-        id: 1,
-        
-    }
-]
+const ListOfProj = styled.div`
+  ul{
+    padding: 0;
+  }
+  li {
+    list-style: none;
+  }
+`;
 
-function Portfolio() {
-    return <div></div>
+class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [],
+    };
+    this.fetchProjects = this.fetchProjects.bind(this);
+  }
+  componentDidMount() {
+    this.fetchProjects();
+  }
+
+  fetchProjects() {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/NicolasBonamy/portfolio/dev/src/Projects.json"
+      )
+      .then((resp) => {
+        this.setState({
+          projects: resp.data,
+        });
+      });
+  }
+
+  render() {
+    const { projects } = this.state;
+    return (
+      <ListOfProj>
+        <ul>
+          {projects.map((project) => {
+            return (
+              <li>
+                <Project {...project} />
+              </li>
+            );
+          })}
+        </ul>
+      </ListOfProj>
+    );
+  }
 }
 
 export default Portfolio;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { Component }from 'react';
+import playerWild from '../pictures/player-wild.png';
+import middleTrade from '../pictures/middle-trade.png';
+import vegenantesGo from '../pictures/vegenantes-go.png';
 
-function Project() {
-    return <div></div>
+
+class Project extends Component {
+    render() {
+        const { date, titre, cadre, source, illustration, description, skills } = this.props;
+        return (
+        <div className="Project">
+            <div>{date}</div>
+            <div>{titre}</div>es
+            <div>{cadre}</div>
+            <div>{source}</div>
+            <img src={illustration} />
+            <div>{description}</div>
+            <div>{skills}</div>
+        </div>
+        );
+    }
 }
 
 export default Project;
